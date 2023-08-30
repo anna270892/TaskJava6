ff787406e914b4f250e847241ac167a20e192d9a
Anna Kochergina <akochergina92@gmail.com>
2023-08-30 21:35:39 +0300

    refactoring

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,11 +1,11 @@
 public class CreditPaymentService {
-    public double calculate (double annualRate, double amountOfCredit, double creditTerm) {  //годовая ставка, сумма кредита, срок кредита
-        double TermInMonths = creditTerm * 12; // срок кредита в месяцах
-        double interestRatePerMonth = annualRate / TermInMonths / 100; // ставка за 1 месяц
+    public double calculate(double annualRate, double amountOfCredit, double creditTerm) {  //годовая ставка, сумма кредита, срок кредита
+        double termInMonths = creditTerm * 12; // срок кредита в месяцах
+        double interestRatePerMonth = annualRate / termInMonths / 100; // ставка за 1 месяц
 
-        double AnnuityRatio = interestRatePerMonth * Math.pow ((1+interestRatePerMonth),TermInMonths) / (Math.pow ((1+interestRatePerMonth),TermInMonths)-1);
-        double MonthlyPayment = AnnuityRatio * amountOfCredit;
-        return MonthlyPayment;
+        double annuityRatio = interestRatePerMonth * Math.pow((1 + interestRatePerMonth), termInMonths) / (Math.pow((1 + interestRatePerMonth), termInMonths) - 1);
+        double monthlyPayment = annuityRatio * amountOfCredit;
+        return monthlyPayment;
     }
 }
 
--- src/Main.java
+++ src/Main.java
@@ -3,13 +3,13 @@ public class Main {
         CreditPaymentService service = new CreditPaymentService();
 
         double calculation1 = service.calculate(9.99, 1000000, 1);
-        System.out.println((int)calculation1);
+        System.out.println((int) calculation1);
 
         double calculation2 = service.calculate(9.99, 1000000, 2);
-        System.out.println((int)calculation2);
+        System.out.println((int) calculation2);
 
         double calculation3 = service.calculate(9.99, 1000000, 3);
-        System.out.println((int)calculation3);
+        System.out.println((int) calculation3);
     }
 }
 
